236259a2ceb7380005863b188b8a9d3a44ad10b7
Omochao <12854316+Omochao@users.noreply.github.com>
2017-09-26 13:43:34 -0400

    Add files via upload

---
+++ Even_Fibonacci.py
@@ -0,0 +1,19 @@
+#By considering the terms in the Fibonacci sequence whose values do not exceed four million,
+#find the sum of the even-valued terms.
+memo = [0,1]
+def fib(n):
+     if n < 2:
+          return n
+     elif n <= len(memo)-1:
+          return memo[n]
+     else:
+          val = fib(n-1)+fib(n-2)
+          memo.append(val)
+          return val
+
+n = 0
+index = 0
+while n < 4000000:
+     n = fib(index)
+     index+=1
+print(sum([fib(n) for n in range(0,index-1,2)]))
